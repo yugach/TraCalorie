b103eb5b887cbbb9e5eca503c038672f19713ffb
Henry Adeyemi <youngach@gmail.com>
2020-05-22 18:17:20 +0100

    fetch items into UI

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ const ItemCtrl = (function () {
 
     // Public Methods
     return {
+        getItems: function () {
+            return data.items;
+        },
         logData: function () {
             return data;
         }
@@ -34,9 +37,27 @@ const ItemCtrl = (function () {
 
 // UI Controller
 const UICtrl = (function () {
+    const UISelectors = {
+        itemList: '#item-list'
+    };
 
     // Public Methods
     return {
+        populateItemList: function (items) {
+            let html = '';
+
+            items.forEach(function (item) {
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>
+            </li>`;
+            });
+
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
 
     };
 
@@ -49,7 +70,13 @@ const App = (function (ItemCtrl, UICtrl) {
     // Public Methods
     return {
         init: function () {
-            console.log('Initializing App...');
+
+            // Fetch items from data structure
+            const items = ItemCtrl.getItems();
+
+            // Populate list with items
+            UICtrl.populateItemList(items);
+
 
         }
     };
