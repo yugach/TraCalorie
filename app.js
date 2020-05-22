f502722ff8c308b10e42c53c8ba7ff708bfd53d2
Henry Adeyemi <youngach@gmail.com>
2020-05-22 17:52:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// Storage Controller
+
+
+// Item Controller
+const ItemCtrl = (function () {
+
+    // Item Constructor
+    const Item = function (id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    };
+
+    // Data Structure / State
+    const data = {
+        items: [
+            { id: 0, name: "Steak Dinner", calories: 1200 },
+            { id: 0, name: "Cookies", calories: 400 },
+            { id: 0, name: "Eggs", calories: 300 }
+        ],
+        currentItem: null,
+        totalCalories: 0
+    };
+
+    // Public Methods
+    return {
+        logData: function () {
+            return data;
+        }
+    };
+
+})();
+
+
+// UI Controller
+const UICtrl = (function () {
+
+    // Public Methods
+    return {
+
+    };
+
+})();
+
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {
+
+    // Public Methods
+    return {
+        init: function () {
+            console.log('Initializing App...');
+
+        }
+    };
+
+
+})(ItemCtrl, UICtrl);
+
+
+// Initialize App
+App.init();
